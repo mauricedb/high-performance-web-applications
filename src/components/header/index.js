ab252e31fe95fcea4330b000074bc47964419ca1
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-06-28 08:35:33 +0200

    Expand header on mobile

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,45 +1,51 @@
-import { h, Component } from 'preact';
-import { Link } from 'preact-router';
-import style from './style';
+import { h, Component } from "preact";
+import { Link } from "preact-router";
+import style from "./style";
 
 export default class Header extends Component {
-	render() {
-return (
-	<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-	  					<Link class="navbar-brand" href="/">Home</Link>
-    </div>
+  state = {
+    expanded: false
+  }
+  toggleExpanded = () => {
+    this.setState({expanded: !this.state.expanded});
+  }
+  collapse = () => {
+    this.setState({expanded: false});
+  }
+  render({}, {expanded}) {
+    const dropDownClass = expanded ? '': 'collapse navbar-collapse';
 
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-		  
-        <li><Link href="/movies">Movies</Link></li>
-        <li><Link href="/directors">Directors</Link></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-);
+    return (
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button
+              type="button"
+              class="navbar-toggle collapsed"
+              data-toggle="collapse"
+              data-target="#bs-example-navbar-collapse-1"
+              aria-expanded="false"
+              onClick={this.toggleExpanded}
+            >
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar" />
+              <span class="icon-bar" />
+              <span class="icon-bar" />
+            </button>
+            <Link class="navbar-brand" href="/">Home</Link>
+          </div>
 
-
-
-		return (
-			<header class={style.header}>
-				<h1>Preact App</h1>
-				<nav>
-					<Link href="/">Home</Link>
-					<Link href="/movies">Movies</Link>
-					<Link href="/profile">Me</Link>
-					<Link href="/profile/john">John</Link>
-				</nav>
-			</header>
-		);
-	}
+          <div
+            class={dropDownClass}
+            id="bs-example-navbar-collapse-1"
+          >
+            <ul class="nav navbar-nav navbar-right">
+              <li><Link onClick={this.collapse} href="/movies">Movies</Link></li>
+              <li><Link onClick={this.collapse} href="/directors">Directors</Link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
